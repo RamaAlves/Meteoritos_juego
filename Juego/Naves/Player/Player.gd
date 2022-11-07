1f44d82fbd40eccee936f84b6a5eabd3a9c80687
RamaAlves <ramiroalvesprof@outlook.com>
2022-11-06 22:17:54 -0300

    Agregando estados Spawn y Vivo

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -2,25 +2,53 @@
 class_name Player
 extends RigidBody2D
 
+##Enums
+enum ESTADO{SPAWN,VIVO,INVENCIBLE,MUERTO}
+
 ##atributos Export
 export var potencia_motor:int = 20
 export var potencia_rotacion:int =240
+export var estela_maxima:int =170
 
 ## atributos
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
+var estado_actual:int=ESTADO.SPAWN
 
 ##Atributos onready
 onready var canion:Canion =$Canion
 onready var laser:RayoLaser=$LaserBeam2D
+onready var estela:Estela=$EstelaPuntoInicio/Trail2D
+onready var motor_sfx:Motor = $MotorSFX
+onready var colisionador:CollisionPolygon2D =$CollisionPolygon2D
 
 ## Metodos
 func _unhandled_input(event: InputEvent) -> void:
+	#validacion de estado y retorno temprano
+	if not esta_input_activo():
+		return
+	
+	#settear estela y sonido del motor
+	if event.is_action_pressed("mover_adelante"):
+		estela.set_max_points(estela_maxima)
+		motor_sfx.sonido_on()
+	elif event.is_action_pressed("mover_atras"):
+		estela.set_max_points(10)
+		motor_sfx.sonido_on()
+	
+	if (event.is_action_released("mover_adelante") or event.is_action_released("mover_atras")):
+		motor_sfx.sonido_off()
+	
 	#disparo rayo
 	if event.is_action_pressed("disparo_secundario"):
 		laser.set_is_casting(true)
 	if event.is_action_released("disparo_secundario"):
 		laser.set_is_casting(false)
+
+func _ready()->void:
+	controlador_estados(estado_actual)
+	##TODO:quitar, solo DEBUG
+	#controlador_estados(ESTADO.VIVO)
 
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
@@ -31,7 +59,35 @@ func _process(delta: float) -> void:
 
 
 ## Metodos Custom
+func controlador_estados(nuevo_estado:int)->void:
+	match nuevo_estado:
+		ESTADO.SPAWN:
+			colisionador.set_deferred("disabled",true)
+			canion.set_puede_disparar(false)
+		ESTADO.VIVO:
+			colisionador.set_deferred("disabled",false)
+			canion.set_puede_disparar(true)
+		ESTADO.INVENCIBLE:
+			colisionador.set_deferred("disabled",true)
+		ESTADO.MUERTO:
+			colisionador.set_deferred("disabled",true)
+			canion.set_puede_disparar(false)
+			queue_free()
+		_:
+			printerr("Error de estado")
+	
+	estado_actual=nuevo_estado
+
+func esta_input_activo()-> bool:
+	if estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]:
+		return false
+	return true
+
 func player_input() -> void:
+	#validacion de estado y retorno temprano
+	if not esta_input_activo():
+		return
+	
 	#Empuje
 	empuje=Vector2.ZERO
 	if Input.is_action_pressed("mover_adelante"):
@@ -52,11 +108,7 @@ func player_input() -> void:
 	if Input.is_action_just_released("disparo_base"):
 		canion.set_esta_disparando(false)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+#signals internas
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name=="Spawn":
+		controlador_estados(ESTADO.VIVO)
---
+++ Juego/Naves/Player/motor/MotorSFX.gd
@@ -0,0 +1,32 @@
+#Motor.gd
+class_name Motor
+extends AudioStreamPlayer2D
+
+export var tiempo_transicion:float = 0.6
+export var volumen_apagado:float = -30.0
+export var volumen_original:float = 0.0
+
+onready var tween_sonido:Tween = $Tween
+
+func ready() -> void:
+	volume_db = volumen_apagado
+
+func sonido_on()->void:
+	if not playing:
+		play()
+	efecto_transicion(volume_db,volumen_original)
+
+func sonido_off()-> void:
+	efecto_transicion(volume_db,volumen_apagado)
+
+func efecto_transicion(desde_vol:float, hasta_vol:float)->void:
+	tween_sonido.interpolate_property(
+		self,
+		"volume_db",
+		desde_vol,
+		hasta_vol,
+		tiempo_transicion,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT_IN
+	)
+	tween_sonido.start()
